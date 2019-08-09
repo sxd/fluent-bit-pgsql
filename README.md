c2f31c525a55ca20e0a75d150bd375a63c78b9dc
Jonathan Gonzalez V <jonathan.abdiel@gmail.com>
2019-08-09 14:26:19 -0400

    Initial commit, just the first draft need a lot of work

---
+++ README.md
@@ -0,0 +1,51 @@
+# Fluent Bit Plugin pgsql
+
+The fallowing repository provides a plugin to send logs to a PostgreSQL database.
+
+## Requirements
+
+- [Fluent Bit](https://fluentbit.io) Source code, version >= 1.2
+- C compiler: GCC or Clang
+- CMake3
+
+## Getting Started
+
+In the following steps we will build the plugin. As a first step get into the _build/_ directory:
+
+```bash
+$ cd build/
+```
+
+Now we will provide CMake (our build system) two important variables:
+
+- FLB\_SOURCE: absolute path to source code of Fluent Bit.
+- PLUGIN\_NAME: _directory_ name of the project that we aim to build. Note that any plugin name must have it proper prefix as the example mentioned above.
+
+Assuming that Fluent Bit source code is located at /tmp/fluent-bit, we will run CMake with the following options:
+
+```bash
+$ cmake -DFLB_SOURCE=/tmp/fluent-bit -DPLUGIN_NAME=out_pgsql ../
+```
+
+then type 'make' to build the plugin:
+
+```
+$ make
+Scanning dependencies of target flb-out_pgsql
+[ 50%] Building C object out_pgsql/CMakeFiles/flb-out_pgsql.dir/pgsql.c.o
+[100%] Linking C shared library ../flb-out_pgsql.so
+[100%] Built target flb-out_pgsql
+```
+
+If you query the content of the current directory you will find the new shared library created:
+
+```
+$ ls -l *.so
+-rwxr-xr-x 1 zeus zeus 37464 Aug  9 13:52 flb-out_pgsql.so
+```
+
+now it can be loaded from Fluent Bit through the [plugins configuration](https://github.com/fluent/fluent-bit/blob/master/conf/plugins.conf) file.
+
+## License
+
+This program is under the terms of the [Apache License v2.0](http://www.apache.org/licenses/LICENSE-2.0).
---
+++ out_pgsql/pgsql.c
@@ -0,0 +1,116 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Fluent Bit
+ *  ==========
+ *  Copyright (C) 2019      Jonathan Gonzalez V. <jonathan.abdiel@gmail.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include <fluent-bit/flb_info.h>
+#include <fluent-bit/flb_output.h>
+#include <fluent-bit/flb_pack.h>
+
+#include "pgsql.h"
+
+static int cb_pgsql_init(struct flb_output_instance *ins,
+                          struct flb_config *config, void *data)
+{
+
+    struct flb_pgsql_config *ctx;
+    const char *conn_str;
+
+    /* default values just in case */
+
+    if(!ins->host.name) {
+        ins->host.name = flb_strdup(FLB_PGSQL_HOST);
+    }
+
+    if(!ins->host.port) {
+        ins->host.port = FLB_PGSQL_PORT;
+    }
+
+    ctx = flb_calloc(1, sizeof(struct flb_pgsql_config));
+    if (!ctx) {
+        flb_errno();
+        return -1;
+    }
+
+    ctx->db_hostname = flb_strdup(ins->host.name);
+    snprintf(ctx->db_port, sizeof(ctx->db_port), "%d", ins->host.port);
+    ctx->db_name = flb_output_get_property("database", ins); /* it should be dbname, may need a fix */
+    ctx->db_user = flb_output_get_property("user", ins);
+    ctx->db_passwd = flb_output_get_property("passwd", ins);
+
+    ctx->conn = PQsetdbLogin(ctx->db_hostname,
+                             ctx->db_port,
+                             NULL, NULL,
+                             ctx->db_name,
+                             ctx->db_user,
+                             ctx->db_passwd);
+
+    if(PQstatus(ctx->conn) != CONNECTION_OK) {
+        flb_debug("[out_pgsql] host=%s port=%s dbname=%s %s",
+                  ctx->db_hostname, ctx->db_port, ctx->db_name, PQerrorMessage(ctx->conn));
+        return -1;
+    }
+
+    flb_debug("[out_pgsql] host=%s port=%s dbname=%s OK",
+              ctx->db_hostname, ctx->db_port, ctx->db_name);
+    flb_output_set_context(ins, ctx);
+
+    return 0;
+}
+
+static void cb_pgsql_flush(const void *data, size_t bytes,
+                            const char *tag, int tag_len,
+                            struct flb_input_instance *i_ins,
+                            void *out_context,
+                            struct flb_config *config)
+{
+    struct flb_pgsql_config *ctx = out_context;
+    PGresult *res;
+    flb_sds_t json;
+    char *query = NULL;
+
+    json = flb_pack_msgpack_to_json_format(data, bytes,
+                                           FLB_PACK_JSON_FORMAT_JSON,
+                                           FLB_PACK_JSON_DATE_DOUBLE,
+                                           flb_sds_create("date"));
+
+    query = flb_malloc(flb_sds_len(json) + 115);
+    snprintf(query, flb_sds_len(json) + 115, "with json_pack as (select json_array_elements('%s') as json_pack )INSERT INTO fluent_log select * from json_pack;", json);
+    res = PQexec(ctx->conn, query);
+
+    if(PQresultStatus(res) != PGRES_COMMAND_OK) {
+        flb_debug("[out_pgsql] %s", PQerrorMessage(ctx->conn));
+        PQclear(res);
+        FLB_OUTPUT_RETURN(FLB_ERROR);
+    }
+
+    FLB_OUTPUT_RETURN(FLB_OK);
+}
+
+static int cb_pgsql_exit(void *data, struct flb_config *config)
+{
+    return 0;
+}
+
+struct flb_output_plugin out_pgsql_plugin = {
+    .name         = "pgsql",
+    .description  = "PostgreSQL database",
+    .cb_init      = cb_pgsql_init,
+    .cb_flush     = cb_pgsql_flush,
+    .cb_exit      = cb_pgsql_exit,
+    .flags        = 0,
+};
